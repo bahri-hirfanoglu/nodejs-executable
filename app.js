31eb3649119f939d2917b4ed84810e0cc7bf751d
bahrihrf34 <bahrihrf34@gmail.com>
2022-08-08 21:56:24 +0300

    express server created

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require("express");
+const express = require("express"),
+path = require('path'),
+app = express();
 
-const app = express();
-
+app.set('views', path.join(__dirname, '/resource/views'));
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/fred.jpg');
+    res.render('index', {dirname: __dirname})
+});
+
+app.get('/assets/*', function(req, res) {
+    console.log(req.path)
+    res.sendFile(__dirname + "/resource/public/" + req.path)
 });
 
 const port = process.env.PORT || 3333;
@@ -13,3 +20,4 @@ const port = process.env.PORT || 3333;
 app.listen(port, () => {
   console.log(`Server started! Listening port ${port}`);
 });
+
